c4fe34651da2fcc197f6b784b7947cd24eafcb3e
Joshua Leath <lawnfurniture@icloud.com>
2021-01-22 10:21:16 -0500

    added redirect to root if invalid path is used

--- book_viewer.rb
+++ book_viewer.rb
@@ -10,6 +10,10 @@ helpers do
   end
 end
 
+not_found do
+  redirect '/'
+end
+
 before do
   @chapter_list = File.readlines('data/toc.txt')
 end
@@ -22,6 +26,7 @@ end
 get "/chapters/:chapter_number" do
   @chapter_list = File.readlines('data/toc.txt')
   @chapter_number = params[:chapter_number].to_i
+  redirect '/' unless (1..@chapter_list.size).cover?(@chapter_number)
   @chapter_title = @chapter_list[@chapter_number]
   @title = "#{@chapter_number}: #{@chapter_title}"
   @paragraphs = File.read("data/chp#{@chapter_number}.txt")
