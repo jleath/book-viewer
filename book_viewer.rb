5515947787ffc84c15a7d820a5a27713e7ede41a
Josh Leath <lawnfurniture@icloud.com>
2021-01-22 10:13:33 -0500

    first commit

---
+++ book_viewer.rb
@@ -0,0 +1,29 @@
+require "sinatra"
+require "sinatra/reloader"
+require "tilt/erubis"
+
+helpers do
+  def in_paragraphs(content)
+    content.split("\n\n")
+           .map { |paragraph| "<p>#{paragraph}</p>" }
+           .join
+  end
+end
+
+before do
+  @chapter_list = File.readlines('data/toc.txt')
+end
+
+get "/" do
+  @title = 'The Adventures of Sherlock Holmes'
+  erb :home
+end
+
+get "/chapters/:chapter_number" do
+  @chapter_list = File.readlines('data/toc.txt')
+  @chapter_number = params[:chapter_number].to_i
+  @chapter_title = @chapter_list[@chapter_number]
+  @title = "#{@chapter_number}: #{@chapter_title}"
+  @paragraphs = File.read("data/chp#{@chapter_number}.txt")
+  erb :chapter
+end
