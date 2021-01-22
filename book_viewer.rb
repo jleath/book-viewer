a5b57441ef4a5440ce97176cfa549ed530d5dae9
Joshua Leath <lawnfurniture@icloud.com>
2021-01-22 11:18:09 -0500

    began implementing search

--- book_viewer.rb
+++ book_viewer.rb
@@ -4,9 +4,9 @@ require "tilt/erubis"
 
 helpers do
   def in_paragraphs(content)
-    content.split("\n\n")
-           .map { |paragraph| "<p>#{paragraph}</p>" }
-           .join
+    content.split("\n\n").each_with_index.map do |paragraph, index|
+      "<p id=paragraph#{index}>#{paragraph}</p>"
+    end.join
   end
 end
 
@@ -16,6 +16,25 @@ end
 
 before do
   @chapter_list = File.readlines('data/toc.txt')
+end
+
+def each_chapter
+  @chapter_list.each_with_index do |title, index|
+    number = index + 1
+    contents = File.read("data/chp#{number}.txt")
+    yield number, title, contents
+  end
+end
+
+def chapters_matching(query)
+  matches = []
+  return matches if query.nil? || query.empty?
+
+  each_chapter do |number, title, contents|
+    matches << {number: number, title: title} if contents.include?(query)
+  end
+
+  matches
 end
 
 get "/" do
@@ -31,4 +50,11 @@ get "/chapters/:chapter_number" do
   @title = "#{@chapter_number}: #{@chapter_title}"
   @paragraphs = File.read("data/chp#{@chapter_number}.txt")
   erb :chapter
+end
+
+get "/search" do
+  @title = 'The Adventures of Sherlock Holmes'
+  @search_query = params[:query]
+  @matches = chapters_matching(@search_query)
+  erb :search
 end
